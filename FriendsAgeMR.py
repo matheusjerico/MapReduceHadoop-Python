6f46b9d1bd3b93796409368c81cc28279a19e6d1
Matheus Jericó <matheusjerico1994@hotmail.com>
2020-01-09 09:48:30 -0300

    avg friends by age

---
+++ FriendsAgeMR.py
@@ -0,0 +1,18 @@
+from mrjob.job import MRJob
+
+class FriendsAgeMR(MRJob):
+    def mapper(self, key, line):
+        (ID, name, age, nFriends) = line.split(',')
+        yield age, float(nFriends)
+    
+    def reducer(self, age, nFriends):
+        count = 0
+        total = 0
+        for x in nFriends:
+            count += 1
+            total += x
+        
+        yield age, (total / count)
+        
+if __name__ == '__main__':
+    FriendsAgeMR.run()
